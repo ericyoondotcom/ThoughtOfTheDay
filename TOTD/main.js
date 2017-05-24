9fc836994b03115923e3944b65f7b48de7b5941a
Eric <emjyoon@gmail.com>
2017-05-23 19:35:47 -0700

    Random thought every tab option
    Now you can choose to have a random thought each tab.

--- TOTD/main.js
+++ TOTD/main.js
@@ -1,4 +1,11 @@
-  $.getJSON("https://raw.githubusercontent.com/yummypasta/ThoughtOfTheDay/master/thoughts.json", function(json){
+    window.onload = onLoad;
+    
+function onLoad(){
+    chrome.storage.sync.get({
+    Night: false,
+    ChangeEveryTab: false
+  }, function(items) {
+    $.getJSON("https://raw.githubusercontent.com/yummypasta/ThoughtOfTheDay/master/thoughts.json", function(json){
     //console.log(json);
     var now = new Date();
     var day = Math.floor(now/8.64e7);
@@ -7,9 +14,15 @@
     console.log("json length is " + json.thoughts.length);
     console.log("so the thought is " + day % json.thoughts.length);
     console.log(totd);
+    if(items.ChangeEveryTab){
+      totd = json.thoughts[Math.floor(Math.random() * json.thoughts.length)];
+      console.log("BUT WAIT! We're overriding because ChangeEveryTab is true!!! The plot thickens!");
+    }
     $("#thought").text(totd.text);
     //console.log(totd.source);
     $("#source").text((totd.source === undefined) ? "" : "Source: " + totd.source);
 
   });
+  });
 
+}
