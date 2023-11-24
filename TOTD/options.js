047ec737093b40262e3a865268881240b3a897a6
Eric Yoon <emjyoon@gmail.com>
2023-11-24 00:36:51 -0500

    Rework entire page design and upgrade to manifest v3

--- TOTD/options.js
+++ TOTD/options.js
@@ -1,35 +1,30 @@
-// Saves options to chrome.storage.sync.
-function save_options() {
-  var nightMode = document.getElementById('nightmode').checked;
-  var tabCycle = document.getElementById('tabcycle').checked;
-  chrome.storage.sync.set({
-    Night: nightMode,
-    ChangeEveryTab: tabCycle
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Changes successfully saved!';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 1500);
-  });
+function saveOptions() {
+	var nightMode = document.getElementById('nightmode').checked;
+	var tabCycle = document.getElementById('tabcycle').checked;
+
+	chrome.storage.sync.set({
+		Night: nightMode,
+		ChangeEveryTab: tabCycle,
+	}, () => {
+		var status = document.getElementById('status');
+		status.textContent = 'Changes successfully saved!';
+		setTimeout(() => {
+			status.textContent = '';
+		}, 1500);
+	});
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    Night: false,
-    ChangeEveryTab: false
-  }, function(items) {
-    document.getElementById('nightmode').checked = items.Night;
-    document.getElementById('tabcycle').checked = items.ChangeEveryTab;
-  });
+function restoreOptions() {
+	chrome.storage.sync.get({
+		Night: false,
+		ChangeEveryTab: false
+	}, (items) => {
+		document.getElementById('nightmode').checked = items.Night;
+		document.getElementById('tabcycle').checked = items.ChangeEveryTab;
+	});
 }
-window.onload = function(){
-    restore_options();
-    document.getElementById('save').addEventListener('click',
-        save_options);
 
+window.onload = function () {
+	restoreOptions();
+	document.getElementById('save').addEventListener('click', saveOptions);
 };
---
+++ TOTD/thoughtPage.js
@@ -0,0 +1,48 @@
+window.onload = () => {
+	chrome.storage.sync.get({
+		Night: false,
+		ChangeEveryTab: false
+	}, function (items) {
+		setRandomColor(items);
+		setText(items);
+	});
+}
+
+function setText(storageItems) {
+	$.getJSON("https://raw.githubusercontent.com/ericyoondotcom/ThoughtOfTheDay/master/thoughts.json", (json) => {
+		const now = new Date();
+		const day = Math.floor(now / 8.64e7);
+
+		const totd = storageItems.ChangeEveryTab ?
+			json.thoughts[Math.floor(Math.random() * json.thoughts.length)] :
+			json.thoughts[day % json.thoughts.length];
+
+		$("#thought").text(totd.text);
+		$("#source").text(totd.source && `Source: ${totd.source}`);
+	});
+}
+
+function setRandomColor(storageItems) {
+	console.log("ROFL");
+	const h1 = Math.floor(Math.random() * 360);
+	const h2 = h1 + Math.floor(Math.random() * 180) - 60;
+	const dark = storageItems.Night;
+	const s = dark ? 1 : 0.20;
+	const v = dark ?
+		Math.floor(Math.random() * 20) :
+		Math.floor(Math.random() * 20) + 80;
+
+	const [r1, g1, b1] = hsv2rgb(h1, s, v / 100);
+	const [r2, g2, b2] = hsv2rgb(h2, s, v / 100);
+
+	$("body").css(
+		"background",
+		`linear-gradient(to bottom left, rgb(${r1}, ${g1}, ${b1}), rgb(${r2}, ${g2}, ${b2}))`);
+	$("body").css("color", dark ? "white" : "black");
+	$("body").css("text-shadow", dark ? "0 0 20px #000000AA" : "0 0 20px #FFFFFFAA");
+}
+
+function hsv2rgb(h, s, v) {
+	let f = (n, k = (n + h / 60) % 6) => v - v * s * Math.max(Math.min(k, 4 - k, 1), 0);
+	return [f(5) * 255, f(3) * 255, f(1) * 255];
+}
